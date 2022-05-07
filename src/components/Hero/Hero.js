59b967910d63207e2618caed69e252d3f6831a0c
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2022-05-07 08:23:47 -0700

    Update Hero.js
    fixed typo in Dapps

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        <h2>I am a new and aspiring developer. I studied with Moralis Academy. I am looking for an opportunity to work as a front end engineer for projects with Ethereum blockchain, Daps, Metaverse, Defi and NFTs.</h2>
+        <h2>I am a new and aspiring developer. I studied with Moralis Academy. I am looking for an opportunity to work as a front end engineer for projects with Ethereum blockchain, Dapps, Metaverse, DAOs, Defi and NFTs.</h2>
       </SectionText>
       {/*  <Button onClick={() => window.location = "https://google.com"} >Learn More</Button> */}
     </LeftSection>
